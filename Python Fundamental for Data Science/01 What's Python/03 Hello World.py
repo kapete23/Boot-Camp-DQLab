70a82d1e36c233da34f9dbb96e9d4b410e3d6b72
kapete23 <kevingokils97@gmail.com>
2021-12-20 16:49:26 +0700

    Menambahkan file .py <03 Hello World> pada folder <01 What's Python>

---
+++ Python Fundamental for Data Science/01 What's Python/03 Hello World.py
@@ -0,0 +1,7 @@
+# Struktur code pada Python relatif sangat sederhana, tidak serumit seperti pada Java ataupun C. 
+# Selain karena memang mudah dibaca, python juga termasuk pada high level programming language
+
+print(10*2+5)
+print("Academy DQLab")
+
+print("\nHello Kevin")
